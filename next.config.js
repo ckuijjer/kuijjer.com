24a553ff3450713b133853d354363909fc7769dc
Casper Kuijjer <casper@kuijjer.com>
2023-04-09 10:07:09 +0200

    github pages

--- next.config.js
+++ next.config.js
@@ -5,11 +5,12 @@ const withNextra = nextra({
   themeConfig: './theme.config.tsx',
   staticImage: true,
   defaultShowCopyCode: true,
-  // readingTime: true
+  // readingTime: true,
 })
 
-// If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
 export default withNextra({
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 })
